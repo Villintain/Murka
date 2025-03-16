563a5e5a3a35423ca531936a46a7808c4925be20
Villintain <sergeyshevchuck@yandex.ru>
2025-03-16 11:10:29 +0300

    sql check

--- main.py
+++ main.py
@@ -57,6 +57,26 @@ bot = commands.Bot(
     )
 
 
+def exists(name_table,element_name,element):
+    conn = sq.connect('DND.db')
+    cursor = conn.cursor()
+    info = cursor.execute("SELECT * FROM ? WHERE ? = ?",(name_table,element_name,element)).fetchall
+    if len(info) != 0:
+        return True
+    else:
+        return False
+
+
+
+
+
+
+
+
+
+
+
+
 
 #КОД
 #StartUP
@@ -84,65 +104,83 @@ async def on_ready():
 
 
 
+
+
+
+
+
+
+#Команды
+
 @bot.slash_command(name="createcompany",description="Создаёт новую компанию")
 async def createcompany(ctx,name_comp:str,master_c:disnake.Member,player:disnake.Member):
     guild = ctx.guild
 
+    info_master = exists("Company","master",str(master_c))
+    info_name = exists("Company","name",name_comp)
 
-    #Создание ролей 
-    await ctx.guild.create_role(name=f"Мастер {name_comp}", color=0xC71585)
-    await ctx.guild.create_role(name=f"Игрок {name_comp}", color=0x349864)
-    roleCompMaster = disnake.utils.get(ctx.guild.roles,name=f'Мастер {name_comp}')
-    roleCompPlayer = disnake.utils.get(ctx.guild.roles,name=f'Игрок {name_comp}')
-    roleMaster = disnake.utils.get(ctx.guild.roles, id = 1350095630575468619)
-    rolePl = disnake.utils.get(ctx.guild.roles, id = 1350095445321453658)
-    
-
-
-    #Выдача ролей
-    await master_c.add_roles(roleMaster)
-    await player.add_roles(rolePl)
-    await master_c.add_roles(roleCompMaster)
-    await player.add_roles(roleCompPlayer)
-    # await guild.create_category(name="Общие")
+    if info_master==True:
+        await ctx.send(f"У {master_c} уже есть компания. :(")    
+    elif info_name == True:
+        await ctx.send(f"Назввание {name_comp} уже занято :(")
+    else:
 
 
 
-
-    #Создание каналов 
-    categary_comp = await ctx.guild.create_category(name=name_comp)
-    await categary_comp.set_permissions(ctx.guild.default_role, read_messages=False)
-    await categary_comp.set_permissions(roleCompMaster, read_messages=True)
-    
-
-    dm_chanel = await guild.create_text_channel(name="ДМ-ская",category=categary_comp)
-    await dm_chanel.set_permissions(ctx.guild.default_role, read_messages=False)
-    await dm_chanel.set_permissions(roleCompMaster, read_messages=True)
+        #Создание ролей 
+        await ctx.guild.create_role(name=f"Мастер {name_comp}", color=0xC71585)
+        await ctx.guild.create_role(name=f"Игрок {name_comp}", color=0x349864)
+        roleCompMaster = disnake.utils.get(ctx.guild.roles,name=f'Мастер {name_comp}')
+        roleCompPlayer = disnake.utils.get(ctx.guild.roles,name=f'Игрок {name_comp}')
+        roleMaster = disnake.utils.get(ctx.guild.roles, id = 1350095630575468619)
+        rolePl = disnake.utils.get(ctx.guild.roles, id = 1350095445321453658)
+        
 
 
-    chat_chanel = await guild.create_text_channel(name="Чат",category=categary_comp)
-    await chat_chanel.set_permissions(ctx.guild.default_role, read_messages=False)
-    await chat_chanel.set_permissions(roleCompPlayer, read_messages=True)
-    await chat_chanel.set_permissions(roleCompMaster, read_messages=True)
-
-    voice_chanel = await guild.create_voice_channel(name="Игра",category=categary_comp)
-    await voice_chanel.set_permissions(ctx.guild.default_role, read_messages=False)
-    await voice_chanel.set_permissions(roleCompPlayer, read_messages=True)
-    await voice_chanel.set_permissions(roleCompMaster, read_messages=True,move_members=True)
-
-
-    private_chanel = await guild.create_voice_channel(name="Приват",category=categary_comp,user_limit=2)
-    await private_chanel.set_permissions(ctx.guild.default_role, read_messages=False)
-    await private_chanel.set_permissions(roleCompMaster, read_messages=True,move_members=True)
+        #Выдача ролей
+        await master_c.add_roles(roleMaster)
+        await player.add_roles(rolePl)
+        await master_c.add_roles(roleCompMaster)
+        await player.add_roles(roleCompPlayer)
+        # await guild.create_category(name="Общие")
 
 
 
-    #SQL Забота
-    conn = sq.connect('DND.db')
-    cursor = conn.cursor()
-    cursor.execute('INSERT INTO Company (guildid,name,master,player) VALUES (?, ?, ?, ?)',(int(guild.id),name_comp,str(master_c),str(player)))
-    conn.commit()
-    conn.close()
+
+        #Создание каналов 
+        categary_comp = await ctx.guild.create_category(name=name_comp)
+        await categary_comp.set_permissions(ctx.guild.default_role, read_messages=False)
+        await categary_comp.set_permissions(roleCompMaster, read_messages=True)
+        
+
+        dm_chanel = await guild.create_text_channel(name="ДМ-ская",category=categary_comp)
+        await dm_chanel.set_permissions(ctx.guild.default_role, read_messages=False)
+        await dm_chanel.set_permissions(roleCompMaster, read_messages=True)
+
+
+        chat_chanel = await guild.create_text_channel(name="Чат",category=categary_comp)
+        await chat_chanel.set_permissions(ctx.guild.default_role, read_messages=False)
+        await chat_chanel.set_permissions(roleCompPlayer, read_messages=True)
+        await chat_chanel.set_permissions(roleCompMaster, read_messages=True)
+
+        voice_chanel = await guild.create_voice_channel(name="Игра",category=categary_comp)
+        await voice_chanel.set_permissions(ctx.guild.default_role, read_messages=False)
+        await voice_chanel.set_permissions(roleCompPlayer, read_messages=True)
+        await voice_chanel.set_permissions(roleCompMaster, read_messages=True,move_members=True)
+
+
+        private_chanel = await guild.create_voice_channel(name="Приват",category=categary_comp,user_limit=2)
+        await private_chanel.set_permissions(ctx.guild.default_role, read_messages=False)
+        await private_chanel.set_permissions(roleCompMaster, read_messages=True,move_members=True)
+
+
+
+        #SQL Забота
+        conn = sq.connect('DND.db')
+        cursor = conn.cursor()
+        cursor.execute('INSERT INTO Company (guildid,name,master,player) VALUES (?, ?, ?, ?)',(int(guild.id),name_comp,str(master_c),str(player)))
+        conn.commit()
+        conn.close()
 
 
 
